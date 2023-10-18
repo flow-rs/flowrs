5375f551afce6a640fde2750674dbb949960a3c7
Antonino Grasso <antonino.grasso@device-insight.com>
2023-10-18 13:39:42 +0200

    fix: start flow script

--- 2_start_flow.sh
+++ 2_start_flow.sh
@@ -2,4 +2,7 @@
 
 #expects to be run from the flowrs project directory within a monorepo setup for the flow-rs project
 #example: ./2_start_flow.sh flow_project_80 libflow_project_80.so
-cd ../flowrs-build/target/debug && ./runner_main --flow ../../flow-projects/$1/target/debug/$2
+
+if [ $# -eq 2 ] ; then
+  cd ../flowrs-build/target/debug && ./runner_main --flow ../../flow-projects/$1/target/debug/$2
+fi

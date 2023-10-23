74f7df0789176a49b8d84ae32665d9b7e64c9b60
NicolasBissig <nicbissig@gmail.com>
2023-10-23 12:07:07 +0200

    fix: Filter out hyper traces to avoid infinite recursion

--- src/analytics/tempo_exporter.rs
+++ src/analytics/tempo_exporter.rs
@@ -1,4 +1,5 @@
 use std::fmt::{Debug, Formatter};
+use opentelemetry::Key;
 use opentelemetry_sdk::export::trace::{ExportResult, SpanData, SpanExporter};
 
 pub struct TempoExporter {
@@ -21,7 +22,18 @@ impl Debug for TempoExporter {
 
 impl SpanExporter for TempoExporter {
     fn export(&mut self, batch: Vec<SpanData>) -> futures_core::future::BoxFuture<'static, ExportResult> {
-        let span_data = opentelemetry_stdout::SpanData::from(batch);
+        // filter out traces from the hyper namespace, to avoid infinite recursion when pushing traces
+        let filtered: Vec<_> = batch.into_iter().filter(|span| {
+            let code = span.attributes.get(&Key::from_static_str("code.namespace"));
+            match code {
+                Some(code) => {
+                    !code.as_str().starts_with("hyper")
+                }
+                None => true
+            }
+        }).collect();
+
+        let span_data = opentelemetry_stdout::SpanData::from(filtered);
 
         let response = self.client.post("http://localhost:4318/v1/traces").json(&span_data).send();
         println!("Response: {:?}", response);

dcd5b9f30819780db648355526882d9ce3e9d06f
mophima <contact@moritzphilippmaier.de>
2024-06-26 16:47:42 +0200

    ✨ implement Display and Debug Traits for Message enum

--- src/comm/messages.rs
+++ src/comm/messages.rs
@@ -1,5 +1,24 @@
-#[derive(Debug)]
+use std::fmt;
+
 pub enum Message {
     StartExecution,
     StopExecution,
+}
+
+impl fmt::Debug for Message {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::StartExecution => write!(f, "[[MESSAGE]: StartExecution]"),
+            Self::StopExecution => write!(f, "[[MESSAGE]: StopExecution]"),
+        }
+    }
+}
+
+impl fmt::Display for Message {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::StartExecution => write!(f, "[[MESSAGE]: StartExecution]"),
+            Self::StopExecution => write!(f, "[[MESSAGE]: StopExecution]"),
+        }
+    }
 }

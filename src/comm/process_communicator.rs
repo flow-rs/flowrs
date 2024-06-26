a26a1a43aab04bc39d01284b5d17e738f80ea375
mophima <contact@moritzphilippmaier.de>
2024-06-26 16:25:40 +0200

    🚧 ✨working on IPC communicator

--- src/comm/process_communicator.rs
+++ src/comm/process_communicator.rs
@@ -1,1 +1,29 @@
+use crate::comm::messages::Message;
+use async_trait::async_trait;
+use tokio::process::{ChildStderr, ChildStdin, ChildStdout};
+use tokio::sync::mpsc::{
+    channel,
+    error::{SendError, TryRecvError},
+};
 
+use super::communication::Communicator;
+
+pub struct ProcessCommunicator {
+    stdin: ChildStdin,
+    stdout: ChildStdout,
+    stderr: ChildStderr,
+}
+
+impl ProcessCommunicator {
+    pub fn new() -> Result<Self, Box<dyn std::error::Error>> {
+        let proc = tokio::process::Command::new("echo").spawn()?;
+        let stdin = proc.stdin.expect("StdIn should be provided");
+        let stdout = proc.stdout.expect("StdOut should be provided");
+        let stderr = proc.stderr.expect("StdErr should be provided");
+        Ok(ProcessCommunicator {
+            stdin: stdin,
+            stdout: stdout,
+            stderr: stderr,
+        })
+    }
+}

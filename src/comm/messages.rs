8b3486759bfd3ec01d91241264b3afa6af880ed9
mophima <contact@moritzphilippmaier.de>
2024-07-02 22:03:05 +0200

    ✨ implement Message and MessageError

--- src/comm/messages.rs
+++ src/comm/messages.rs
@@ -1,15 +1,31 @@
 use std::fmt;
 
+#[derive(Debug)]
+pub enum MessageError {
+    CouldNotParse,
+}
+
+impl std::error::Error for MessageError {}
+
+impl fmt::Display for MessageError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
 pub enum Message {
     StartExecution,
     StopExecution,
 }
 
+const START_EXECUTION: &str = "[[MESSAGE]: StartExecution]";
+const STOP_EXECUTION: &str = "[[MESSAGE]: StopExecution]";
+
 impl fmt::Debug for Message {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Self::StartExecution => write!(f, "[[MESSAGE]: StartExecution]"),
-            Self::StopExecution => write!(f, "[[MESSAGE]: StopExecution]"),
+            Self::StartExecution => write!(f, "{}", START_EXECUTION),
+            Self::StopExecution => write!(f, "{}", STOP_EXECUTION),
         }
     }
 }
@@ -17,8 +33,18 @@ impl fmt::Debug for Message {
 impl fmt::Display for Message {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Self::StartExecution => write!(f, "[[MESSAGE]: StartExecution]"),
-            Self::StopExecution => write!(f, "[[MESSAGE]: StopExecution]"),
+            Self::StartExecution => write!(f, "{}", START_EXECUTION),
+            Self::StopExecution => write!(f, "{}", STOP_EXECUTION),
+        }
+    }
+}
+
+impl Message {
+    pub fn from_str(s: &str) -> Option<Self> {
+        match s {
+            START_EXECUTION => Some(Self::StartExecution),
+            STOP_EXECUTION => Some(Self::StopExecution),
+            _ => None,
         }
     }
 }

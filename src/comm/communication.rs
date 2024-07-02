679824a25868ed3f02f4e8d71595845e6692eb95
mophima <contact@moritzphilippmaier.de>
2024-07-02 22:02:32 +0200

    ✨ changed Communicator trait function signatures

--- src/comm/communication.rs
+++ src/comm/communication.rs
@@ -4,6 +4,6 @@ use async_trait::async_trait;
 
 #[async_trait]
 pub trait Communicator {
-    async fn send(&self, message: Message) -> Result<(), Box<dyn std::error::Error>>;
-    fn receive(&mut self) -> Result<Message, Box<dyn std::error::Error>>;
+    async fn send(&mut self, message: Message) -> Result<(), Box<dyn std::error::Error>>;
+    async fn receive(&mut self) -> Result<Message, Box<dyn std::error::Error>>;
 }

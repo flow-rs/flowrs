913e6d03e507778f26a61a110e53acb84ffb613e
mophima <contact@moritzphilippmaier.de>
2024-08-30 15:07:17 +0200

    ✨ implement data Wrapper for data messaging

--- src/comm/data.rs
+++ src/comm/data.rs
@@ -1,16 +1,53 @@
-use std::fmt;
+use std::{fmt, str::FromStr};
 
 #[derive(Debug, PartialEq)]
-pub struct DataWrapper {}
+//Wraps a single data point of type D
+pub struct DataWrapper<D>
+where
+    D: Clone,
+    D: fmt::Debug,
+    D: FromStr,
+{
+    data: D,
+}
 
-impl fmt::Display for DataWrapper {
+impl<D> DataWrapper<D>
+where
+    D: Clone,
+    D: fmt::Debug,
+    D: FromStr,
+{
+    pub fn parse(str: String) -> Result<Self, D::Err> {
+        let res = str.parse::<D>()?;
+        Ok(DataWrapper::<D> { data: res })
+    }
+
+    pub fn get_data(&self) -> D {
+        self.data.clone()
+    }
+}
+
+impl<D> fmt::Display for DataWrapper<D>
+where
+    D: Clone,
+    D: fmt::Debug,
+    D: FromStr,
+{
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{:?}", self)
     }
 }
 
-impl DataWrapper {
-    pub fn parse(_str: String) -> Self {
-        todo!()
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_primitives() {
+        let data_wrapper_1 = DataWrapper::<u32> { data: 3 };
+        let data_wrapper_2_res = DataWrapper::<u32>::parse("3".to_string());
+        assert!(data_wrapper_2_res.is_ok());
+        let data_wrapper_2 = data_wrapper_2_res.unwrap();
+        assert_eq!(data_wrapper_1.get_data(), data_wrapper_2.get_data());
     }
 }

ed1e023f33675ec32fda4e1d4580426af63ee525
mophima <contact@moritzphilippmaier.de>
2025-03-11 09:35:00 +0100

    ✅ fix message tests

--- src/comm/messages.rs
+++ src/comm/messages.rs
@@ -64,10 +64,12 @@ where
 pub const START_EXECUTION: &str = "[[MESSAGE]: StartExecution]";
 pub const STOP_EXECUTION: &str = "[[MESSAGE]: StopExecution]";
 pub const SETUP_COMMUNICATION_PREFIX: &str = "[[MESSAGE]: SetupCommunication]>";
+pub const SETUP_COMMUNICATION_COMM: &str = "[Communicator:[";
+pub const SETUP_COMMUNICATION_TYPE: &str = "], Type:[";
 pub const SETUP_COMMUNICATION_PORT_PREFIX: &str = "[[MESSAGE]: SetupCommunicationPort]>";
 pub const ACKNOWLEDGE_CONNECTION: &str = "[[MESSAGE]: AcknowledgeConnection]";
-
-// New Constants
+pub const DEBUG: &str = "[[MESSAGE]: [DEBUG]>]";
+pub const DATA: &str = "[[MESSAGE]: [DATA]>]";
 pub const INITIALIZE_LOCAL_NODES: &str = "[[MESSAGE]: InitializeLocalNodes]";
 pub const ACKNOWLEDGE_NODE_INITIALIZATION: &str = "[[MESSAGE]: AcknowledgeNodeInitialization]";
 pub const ORCHESTRATOR_REQUEST_NODE_CONNECTION: &str =
@@ -89,6 +91,9 @@ const PATTERNS: &[&str] = &[
     REJECT_PEER_CONNECTION,
     ACKNOWLEDGE_CONNECTION_SETUP,
     SETUP_COMMUNICATION_PREFIX,
+    DEBUG,
+    DATA,
+    SETUP_COMMUNICATION_COMM,
     SETUP_COMMUNICATION_PORT_PREFIX,
     ACKNOWLEDGE_CONNECTION,
 ];
@@ -102,13 +107,32 @@ where
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
+            // Execution Commands
             Message::StartExecution => write!(f, "{}", START_EXECUTION),
             Message::StopExecution => write!(f, "{}", STOP_EXECUTION),
+
+            // Connection Setup
+            Message::SetupCommunication(comm) => write!(
+                f,
+                "{}{}{:?}{}{:?}]",
+                SETUP_COMMUNICATION_PREFIX,
+                SETUP_COMMUNICATION_COMM,
+                comm.communicator,
+                SETUP_COMMUNICATION_TYPE,
+                comm.node_type
+            ),
+            Message::SetupCommunicationPort(port) => {
+                write!(f, "{} {}", SETUP_COMMUNICATION_PORT_PREFIX, port)
+            }
+            Message::AcknowledgeConnection => write!(f, "{}", ACKNOWLEDGE_CONNECTION),
+
+            // Node Initialization
             Message::InitializeLocalNodes => write!(f, "{}", INITIALIZE_LOCAL_NODES),
             Message::AcknowledgeNodeInitialization => {
                 write!(f, "{}", ACKNOWLEDGE_NODE_INITIALIZATION)
             }
 
+            // P2P Connection Coordination
             Message::OrchestratorRequestNodeConnection(n1, n2, r_id, r_ip) => {
                 write!(
                     f,
@@ -125,7 +149,7 @@ where
                     n2,
                     o_idx,
                     i_idx,
-                    serde_json::to_string(dtype).unwrap() // Ensure `dtype` is serialized correctly
+                    serde_json::to_string(dtype).unwrap()
                 )
             }
             Message::AcceptPeerConnection(n1, n2, recv_port) => {
@@ -141,7 +165,10 @@ where
                     ACKNOWLEDGE_CONNECTION_SETUP, n1, n2, recv_port
                 )
             }
-            _ => write!(f, "[[MESSAGE]: UNKNOWN]"),
+
+            // Debugging & Data Transfer
+            Message::Debug(msg) => write!(f, "[[MESSAGE]: [DEBUG]>]{}", msg),
+            Message::Data(data) => write!(f, "[[MESSAGE]: [DATA]>]{:?}", data),
         }
     }
 }
@@ -176,80 +203,267 @@ where
             .unwrap();
 
         match Self::aho_corasick_match(&ac, s) {
+            // Execution Commands
             Some(START_EXECUTION) => Some(Self::StartExecution),
             Some(STOP_EXECUTION) => Some(Self::StopExecution),
+
+            // Connection Setup
+            Some(SETUP_COMMUNICATION_PREFIX) => {
+                let comm_start = s.find(SETUP_COMMUNICATION_COMM)? + SETUP_COMMUNICATION_COMM.len();
+                let comm_end = s.find(SETUP_COMMUNICATION_TYPE)?;
+                let comm_string = s[comm_start..comm_end].to_string();
+                let communicator_option = NodeCommunicator::from_str(&comm_string);
+                if communicator_option.is_none() {
+                    return None;
+                }
+                let communicator = communicator_option.unwrap();
+                let node_type_start = comm_end + SETUP_COMMUNICATION_TYPE.len();
+                let node_type_end = s.rfind(']')?;
+                let node_type_string = s[node_type_start..node_type_end].to_string();
+                println!("{}", node_type_string);
+                let node_type: Type =
+                    serde_json::from_str(&node_type_string).expect("should deserialize");
+                Some(Self::SetupCommunication(CommWrapper {
+                    communicator: communicator,
+                    node_type: node_type,
+                }))
+            }
+            Some(SETUP_COMMUNICATION_PORT_PREFIX) => {
+                let port_string = s
+                    .replacen(SETUP_COMMUNICATION_PORT_PREFIX, "", 1)
+                    .trim()
+                    .to_string();
+                println!("[DEBUG] Extracted Port String: '{}'", port_string.clone());
+                let port = port_string.parse::<u16>().ok()?;
+                Some(Self::SetupCommunicationPort(port))
+            }
+            Some(ACKNOWLEDGE_CONNECTION) => Some(Self::AcknowledgeConnection),
+
+            // Node Initialization
             Some(INITIALIZE_LOCAL_NODES) => Some(Self::InitializeLocalNodes),
             Some(ACKNOWLEDGE_NODE_INITIALIZATION) => Some(Self::AcknowledgeNodeInitialization),
 
+            // P2P Connection Coordination
             Some(ORCHESTRATOR_REQUEST_NODE_CONNECTION) => {
                 let stripped_msg = s.replacen(ORCHESTRATOR_REQUEST_NODE_CONNECTION, "", 1);
                 let parts: Vec<&str> = stripped_msg.split(',').collect();
                 if parts.len() == 4 {
-                    return Some(Self::OrchestratorRequestNodeConnection(
+                    Some(Self::OrchestratorRequestNodeConnection(
                         parts[0].parse().ok()?,
                         parts[1].parse().ok()?,
                         parts[2].parse().ok()?,
                         parts[3].to_string(),
-                    ));
+                    ))
                 } else {
-                    return None;
+                    None
                 }
             }
             Some(REQUEST_PEER_CONNECTION) => {
                 let stripped_msg = s.replacen(REQUEST_PEER_CONNECTION, "", 1);
-                let parts: Vec<&str> = stripped_msg.split(',').collect();
+                let parts: Vec<&str> = stripped_msg.splitn(5, ',').collect();
                 if parts.len() == 5 {
+                    let dtype_str = parts[4].trim();
+                    let dtype: Type = match serde_json::from_str(dtype_str) {
+                        Ok(parsed) => parsed,
+                        Err(_) => return None,
+                    };
+
                     return Some(Self::RequestPeerConnection(
                         parts[0].parse().ok()?,
                         parts[1].parse().ok()?,
                         parts[2].parse().ok()?,
                         parts[3].parse().ok()?,
-                        serde_json::from_str(parts[4]).ok()?,
+                        dtype,
                     ));
-                } else {
-                    return None;
                 }
+                None
             }
             Some(ACCEPT_PEER_CONNECTION) => {
                 let stripped_msg = s.replacen(ACCEPT_PEER_CONNECTION, "", 1);
                 let parts: Vec<&str> = stripped_msg.split(',').collect();
                 if parts.len() == 3 {
-                    return Some(Self::AcceptPeerConnection(
+                    Some(Self::AcceptPeerConnection(
                         parts[0].parse().ok()?,
                         parts[1].parse().ok()?,
                         parts[2].parse().ok()?,
-                    ));
+                    ))
                 } else {
-                    return None;
+                    None
                 }
             }
             Some(REJECT_PEER_CONNECTION) => {
                 let stripped_msg = s.replacen(REJECT_PEER_CONNECTION, "", 1);
                 let parts: Vec<&str> = stripped_msg.split(',').collect();
                 if parts.len() == 3 {
-                    return Some(Self::RejectPeerConnection(
+                    Some(Self::RejectPeerConnection(
                         parts[0].parse().ok()?,
                         parts[1].parse().ok()?,
                         parts[2].to_string(),
-                    ));
+                    ))
                 } else {
-                    return None;
+                    None
                 }
             }
             Some(ACKNOWLEDGE_CONNECTION_SETUP) => {
                 let stripped_msg = s.replacen(ACKNOWLEDGE_CONNECTION_SETUP, "", 1);
                 let parts: Vec<&str> = stripped_msg.split(',').collect();
                 if parts.len() == 3 {
-                    return Some(Self::AcknowledgeConnectionSetup(
+                    Some(Self::AcknowledgeConnectionSetup(
                         parts[0].parse().ok()?,
                         parts[1].parse().ok()?,
                         parts[2].parse().ok()?,
-                    ));
+                    ))
                 } else {
-                    return None;
+                    None
                 }
             }
+
+            // Debugging & Data Transfer
+            Some("[[MESSAGE]: [DEBUG]>]") => {
+                Some(Self::Debug(s.replacen("[[MESSAGE]: [DEBUG]>]", "", 1)))
+            }
+            Some("[[MESSAGE]: [DATA]>]") => {
+                DataWrapper::parse(s.replacen("[[MESSAGE]: [DATA]>]", "", 1))
+                    .ok()
+                    .map(Self::Data)
+            }
+
+            // Default case: unknown message
             _ => None,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::comm::communication::CommWrapper;
+    use crate::comm::thread_communicator::ThreadCommunicator;
+    use crate::flow::flow_types::NodeId;
+    use crate::sched::scheduling_config::RuntimeId;
+    use flowrs_package::flow_package::package::Type;
+
+    /// Example Type for Serialization Testing
+    const TYPE_JSON: &str = r#"
+    {
+        "inputs": null,
+        "outputs": null,
+        "type_parameters": [{"name": "U", "where": []}, {"name": "T", "where": []}],
+        "constructors":{
+            "New":{"NewWithObserver": {}},
+            "FromCode":{"FromCode":{"code_template": "let {{fully_qualified_name}}:{{type_parameter_U}} = 5;"}}
+        }
+    }
+    "#;
+
+    #[tokio::test]
+    async fn test_from_str() {
+        if let Some(start_msg) = Message::<u32>::from_str(START_EXECUTION) {
+            assert_eq!(start_msg, Message::StartExecution)
+        }
+        if let Some(stop_msg) = Message::<u32>::from_str(STOP_EXECUTION) {
+            assert_eq!(stop_msg, Message::StopExecution)
+        }
+        if let Some(debug_msg) = Message::<u32>::from_str(DEBUG) {
+            assert_eq!(debug_msg, Message::Debug("".to_string()))
+        }
+        // if let Some(data_msg) = Message::from_str(DATA) {
+        //     assert_eq!(data_msg, Message::Data(DataWrapper {}))
+        // }
+        let comm = NodeCommunicator::ThreadComm(
+            ThreadCommunicator::<u32>::new().expect("should construct"),
+        );
+        let node_type = serde_json::from_str(TYPE_JSON).expect("should deserialize");
+        let comm_wrapper = CommWrapper {
+            communicator: comm,
+            node_type: node_type,
+        };
+        let format_str = format!(
+            "{}{}{}{}{}]",
+            SETUP_COMMUNICATION_PREFIX,
+            SETUP_COMMUNICATION_COMM,
+            comm_wrapper.communicator,
+            SETUP_COMMUNICATION_TYPE,
+            serde_json::to_string(&(comm_wrapper.node_type)).expect("should serialize"),
+        );
+        if let Some(setup_communication_msg) = Message::<u32>::from_str(&format_str) {
+            assert_eq!(
+                setup_communication_msg,
+                Message::SetupCommunication(comm_wrapper)
+            );
+        }
+        if let Some(setup_communication_port_msg) =
+            Message::<u32>::from_str(format!("{}5050", SETUP_COMMUNICATION_PORT_PREFIX).as_str())
+        {
+            assert_eq!(
+                setup_communication_port_msg,
+                Message::SetupCommunicationPort(5050)
+            )
+        }
+        if let Some(acknowledge_connection_msg) = Message::<u32>::from_str(ACKNOWLEDGE_CONNECTION) {
+            assert_eq!(acknowledge_connection_msg, Message::AcknowledgeConnection)
+        }
+    }
+
+    #[test]
+    fn test_p2p_connection_messages() {
+        let n1: NodeId = 1;
+        let n2: NodeId = 2;
+        let r_id: RuntimeId = 3;
+        let r_ip = "192.168.1.1".to_string();
+        let dtype: Type = serde_json::from_str(TYPE_JSON).unwrap();
+
+        let msg_str = format!(
+            "{}{},{},{},{}",
+            ORCHESTRATOR_REQUEST_NODE_CONNECTION, n1, n2, r_id, r_ip
+        );
+        assert_eq!(
+            Message::<String>::from_str(&msg_str),
+            Some(Message::OrchestratorRequestNodeConnection(
+                n1,
+                n2,
+                r_id,
+                r_ip.clone()
+            ))
+        );
+
+        let msg_str = format!(
+            "{}{},{},{},{},{}",
+            REQUEST_PEER_CONNECTION,
+            n1,
+            n2,
+            0,
+            1,
+            serde_json::to_string(&dtype).unwrap()
+        );
+        assert_eq!(
+            Message::<String>::from_str(&msg_str),
+            Some(Message::RequestPeerConnection(n1, n2, 0, 1, dtype.clone()))
+        );
+
+        let msg_str = format!("{}{},{},{}", ACCEPT_PEER_CONNECTION, n1, n2, 5050);
+        assert_eq!(
+            Message::<String>::from_str(&msg_str),
+            Some(Message::AcceptPeerConnection(n1, n2, 5050))
+        );
+
+        let msg_str = format!(
+            "{}{},{},{}",
+            REJECT_PEER_CONNECTION, n1, n2, "Invalid Connection"
+        );
+        assert_eq!(
+            Message::<String>::from_str(&msg_str),
+            Some(Message::RejectPeerConnection(
+                n1,
+                n2,
+                "Invalid Connection".to_string()
+            ))
+        );
+
+        let msg_str = format!("{}{},{},{}", ACKNOWLEDGE_CONNECTION_SETUP, n1, n2, 5050);
+        assert_eq!(
+            Message::<String>::from_str(&msg_str),
+            Some(Message::AcknowledgeConnectionSetup(n1, n2, 5050))
+        );
     }
 }

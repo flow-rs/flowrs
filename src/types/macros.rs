29128291bc68bfbe9e085555b466198f8ca4037a
mophima <contact@moritzphilippmaier.de>
2025-03-23 21:54:30 +0100

    ⚗️

--- src/types/macros.rs
+++ src/types/macros.rs
@@ -1,40 +1,42 @@
 #[macro_export]
 macro_rules! generate_local_connection {
     ($type:ty) => {
-        // Generate a top-level function for connecting nodes
-        #[allow(non_snake_case)]
-        pub fn connect_nodes_$type(
-            sender_id: NodeId,
-            receiver_id: NodeId,
-            sender_out_idx: NodeIOIndex,
-            recv_in_idx: NodeIOIndex,
-            sender_io: &mut dyn Any,
-            receiver_io: &mut dyn Any,
-        ) {
-            if let Some(sender_output) = sender_io.downcast_mut::<TypedOutput<$type>>() {
-                if let Some(receiver_input) = receiver_io.downcast_mut::<TypedInput<$type>>() {
-                    let (send_half, recv_half) = sender_output.split(sender_out_idx);
-                    receiver_input.set_any_communicator(Box::new(recv_half));
-                    sender_output.set_any_communicator(Box::new(send_half));
+        paste::paste! {
+            // Define the connection function at the top level
+            pub fn [<connect_nodes_ $type>](
+                sender_id: NodeId,
+                receiver_id: NodeId,
+                sender_out_idx: NodeIOIndex,
+                recv_in_idx: NodeIOIndex,
+                sender_io: &mut dyn Any,
+                receiver_io: &mut dyn Any,
+            ) {
+                if let Some(sender_output) = sender_io.downcast_mut::<TypedOutput<$type>>() {
+                    if let Some(receiver_input) = receiver_io.downcast_mut::<TypedInput<$type>>() {
+                        let (send_half, recv_half) = sender_output.split(sender_out_idx);
+                        receiver_input.set_any_communicator(Box::new(recv_half));
+                        sender_output.set_any_communicator(Box::new(send_half));
 
-                    println!(
-                        "[generate_local_connection] Successfully connected nodes {} -> {} with type {}",
-                        sender_id, receiver_id, stringify!($type)
-                    );
+                        println!(
+                            "[generate_local_connection] Successfully connected nodes {} -> {} with type {}",
+                            sender_id, receiver_id, stringify!($type)
+                        );
+                    } else {
+                        panic!("[generate_local_connection] Receiver IO type mismatch");
+                    }
                 } else {
-                    panic!("[generate_local_connection] Receiver IO type mismatch");
+                    panic!("[generate_local_connection] Sender IO type mismatch");
                 }
-            } else {
-                panic!("[generate_local_connection] Sender IO type mismatch");
             }
-        }
 
-        // Register the connection function in the type registry
-        lazy_static::lazy_static! {
-            static ref REGISTER: () = {
-                let func: ConnectionFn = connect_nodes_$type;
-                TYPE_REGISTRY.lock().unwrap().register::<$type>(func);
-            };
+            // Register the connection function in the type registry
+            lazy_static::lazy_static! {
+                static ref REGISTER: () = {
+                    let func: ConnectionFn = [<connect_nodes_ $type>];
+                    TYPE_REGISTRY.lock().unwrap().register::<$type>(func);
+                    println!("[generate_local_connection] Registered connection function for type: {}", stringify!($type));
+                };
+            }
         }
     };
 }

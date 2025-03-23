51254e11556b3396bf6ac93b54f7a9251302c16d
mophima <contact@moritzphilippmaier.de>
2025-03-23 21:48:12 +0100

    ⚗️

--- src/types/macros.rs
+++ src/types/macros.rs
@@ -1,30 +1,38 @@
 #[macro_export]
 macro_rules! generate_local_connection {
     ($type:ty) => {
-        paste::item! {
-            pub fn [<connect_nodes_ $type>](sender_id: NodeId, receiver_id: NodeId, sender_out_idx: NodeIOIndex, recv_in_idx: NodeIOIndex, sender_io: &mut dyn Any, receiver_io: &mut dyn Any) {
-                println!("[generate_local_connection] Attempting to connect nodes with type: {:?}", std::any::TypeId::of::<$type>());
+        paste::paste! {
+            #[allow(non_snake_case)]
+            pub fn [<connect_nodes_ $type>](
+                sender_id: NodeId,
+                receiver_id: NodeId,
+                sender_out_idx: NodeIOIndex,
+                recv_in_idx: NodeIOIndex,
+                sender_io: &mut dyn Any,
+                receiver_io: &mut dyn Any,
+            ) {
+                if let Some(sender_output) = sender_io.downcast_mut::<TypedOutput<$type>>() {
+                    if let Some(receiver_input) = receiver_io.downcast_mut::<TypedInput<$type>>() {
+                        let (send_half, recv_half) = sender_output.split(sender_out_idx);
+                        receiver_input.set_any_communicator(Box::new(recv_half));
+                        sender_output.set_any_communicator(Box::new(send_half));
 
-                // Retrieve the sender output
-                if let Some(typed_sender_io) = sender_io.downcast_mut::<TypedOutput<$type>>() {
-                    println!("[generate_local_connection] Successfully downcasted sender IO to TypedOutput<{}>", stringify!($type));
-
-                    let (send_half, recv_half) = typed_sender_io.split(sender_out_idx);
-
-                    // Set sender communicator
-                    typed_sender_io.set_any_communicator(Box::new(send_half) as Box<dyn Any + Send>);
-
-                    // Retrieve the receiver input
-                    if let Some(typed_receiver_io) = receiver_io.downcast_mut::<TypedInput<$type>>() {
-                        println!("[generate_local_connection] Successfully downcasted receiver IO to TypedInput<{}>", stringify!($type));
-                        typed_receiver_io.set_any_communicator(Box::new(recv_half) as Box<dyn Any + Send>);
-                        println!("[generate_local_connection] Successfully connected nodes: {} -> {}", sender_id, receiver_id);
+                        println!("[generate_local_connection] Successfully connected nodes {} -> {} with type {:?}", sender_id, receiver_id, stringify!($type));
                     } else {
                         panic!("[generate_local_connection] Receiver IO type mismatch");
                     }
                 } else {
                     panic!("[generate_local_connection] Sender IO type mismatch");
                 }
+
+                println!("[generate_local_connection] Local nodes connected: {} -> {} (Out {} -> In {})", sender_id, receiver_id, sender_out_idx, recv_in_idx);
+            }
+
+            // Register the connection function in the type registry
+            lazy_static::lazy_static! {
+                static ref REGISTER: () = {
+                    TYPE_REGISTRY.lock().unwrap().register::<$type>([<connect_nodes_ $type>]);
+                };
             }
         }
     };

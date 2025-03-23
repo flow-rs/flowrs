9ac71bb06cc8d7e3eff2dc967f186566d64de045
mophima <contact@moritzphilippmaier.de>
2025-03-23 15:58:41 +0100

    🎨

--- src/types/macros.rs
+++ src/types/macros.rs
@@ -1,4 +1,59 @@
 #[macro_export]
+macro_rules! generate_local_connection {
+    ($fn_name:ident, $type:ty) => {
+        pub fn $fn_name(
+            sender_id: NodeId,
+            receiver_id: NodeId,
+            sender_out_idx: NodeIOIndex,
+            recv_in_idx: NodeIOIndex,
+            sender_io: &mut dyn Any,
+            receiver_io: &mut dyn Any,
+        ) {
+            println!(
+                "[Node RT] Connecting local nodes {} -> {} (Out {} -> In {}) with type {}",
+                sender_id,
+                receiver_id,
+                sender_out_idx,
+                recv_in_idx,
+                stringify!($type)
+            );
+
+            // Downcast the sender and receiver IO
+            let sender_output = sender_io
+                .downcast_mut::<TypedOutput<$type>>()
+                .expect("Sender output type mismatch");
+
+            let receiver_input = receiver_io
+                .downcast_mut::<TypedInput<$type>>()
+                .expect("Receiver input type mismatch");
+
+            // Local handling (using thread communicators)
+            let comm = sender_output
+                .output
+                .get_communicator_mut()
+                .expect("No communicator found");
+            let send_half = comm.clone_send();
+            let recv_half = comm.move_recv().expect("Failed to move receiver");
+
+            // Assign communicators
+            sender_output
+                .output
+                .set_communicator(NodeCommunicator::ThreadComm(send_half));
+            receiver_input
+                .input
+                .set_communicator(NodeCommunicator::ThreadComm(recv_half));
+
+            println!(
+                "[Node RT] Successfully connected local nodes {} -> {} with type {}",
+                sender_id,
+                receiver_id,
+                stringify!($type)
+            );
+        }
+    };
+}
+
+#[macro_export]
 macro_rules! connect_nodes {
     ($type:ty, $flow:expr, $sender_id:expr, $recv_id:expr, $sender_out_idx:expr, $recv_in_idx:expr) => {{
         // Generate the connection function

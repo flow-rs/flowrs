f41f7cb7ce41d92128b5f7b67312cbf4eb58995c
mophima <contact@moritzphilippmaier.de>
2025-04-17 18:28:46 +0200

    ♻️ change poll_fn to be thread safe with mutex

--- src/types/type_registry.rs
+++ src/types/type_registry.rs
@@ -21,6 +21,7 @@ use std::fmt::Debug;
 use std::future::Future;
 use std::pin::Pin;
 use std::str::FromStr;
+use std::sync::Arc;
 use tokio::sync::Mutex;
 
 use crate::flow::flow_types::{NodeIOIndex, NodeId};
@@ -109,7 +110,7 @@ impl<T: 'static + Send + Sync + Debug + FromStr> CommunicatorBox for NetworkComm
 pub struct TypeRegistry {
     connections: HashMap<TypeId, ConnectionFn>,
     communicator_factories: HashMap<TypeId, CommunicatorFactory>,
-    poll_fns: HashMap<TypeId, Box<dyn Any + Send>>,
+    poll_fns: HashMap<TypeId, Arc<Mutex<Box<dyn Any + Send>>>>,
     pub name_to_id: HashMap<String, TypeId>,
     input_setters: HashMap<TypeId, InputSetterFn>,
     output_setters_with_connect: HashMap<TypeId, OutputSetterWithConnectFn>,
@@ -283,18 +284,18 @@ impl TypeRegistry {
             })
         });
 
-        self.poll_fns
-            .insert(type_id, Box::new(func) as Box<dyn Any + Send>);
+        self.poll_fns.insert(
+            type_id,
+            Arc::new(Mutex::new(Box::new(func) as Box<dyn Any + Send>)),
+        );
     }
 
     /// Get a mutable reference to a polling function for a given type
-    pub fn get_poll_fn<T>(&mut self) -> Option<&mut PollFn<T>>
+    pub fn get_poll_fn<T>(&mut self) -> Option<Arc<Mutex<Box<dyn Any + Send>>>>
     where
         T: 'static + Send + Clone + Debug + FromStr,
     {
-        self.poll_fns
-            .get_mut(&TypeId::of::<T>())?
-            .downcast_mut::<PollFn<T>>()
+        self.poll_fns.get(&TypeId::of::<T>()).cloned()
     }
 }
 

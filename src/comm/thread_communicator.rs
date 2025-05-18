153b712292e5a93e846775a08437e7e7a798e297
mophima <contact@moritzphilippmaier.de>
2025-05-18 21:53:24 +0200

    ⚗️

--- src/comm/thread_communicator.rs
+++ src/comm/thread_communicator.rs
@@ -199,150 +199,150 @@ where
     }
 }
 
-#[cfg(test)]
-mod tests {
+// #[cfg(test)]
+// mod tests {
 
-    use std::sync::{Arc, Mutex};
-    use std::thread::{sleep, spawn};
-    use std::time::Duration;
-    use std::time::Instant;
+//     use std::sync::{Arc, Mutex};
+//     use std::thread::{sleep, spawn};
+//     use std::time::Duration;
+//     use std::time::Instant;
 
-    use tokio::runtime::Runtime;
-    use tokio::sync::mpsc::error::TryRecvError;
+//     use tokio::runtime::Runtime;
+//     use tokio::sync::mpsc::error::TryRecvError;
 
-    use super::*;
+//     use super::*;
 
-    #[tokio::test]
-    async fn test_empty_receive() {
-        let mut communicator = ThreadCommunicator::<u32>::new().unwrap();
-        let res = communicator.try_receive().await;
-        assert!(
-            res.is_err(),
-            "receive() should return an error when the channel is empty."
-        );
+//     #[tokio::test]
+//     async fn test_empty_receive() {
+//         let mut communicator = ThreadCommunicator::<u32>::new().unwrap();
+//         let res = communicator.try_receive().await;
+//         assert!(
+//             res.is_err(),
+//             "receive() should return an error when the channel is empty."
+//         );
 
-        let err = res.unwrap_err();
-        assert!(matches!(
-            err.downcast_ref::<TryRecvError>(),
-            Some(TryRecvError::Empty)
-        ));
-    }
+//         let err = res.unwrap_err();
+//         assert!(matches!(
+//             err.downcast_ref::<TryRecvError>(),
+//             Some(TryRecvError::Empty)
+//         ));
+//     }
 
-    #[tokio::test]
-    async fn test_send() {
-        let mut communicator = ThreadCommunicator::<u32>::new().unwrap();
-        let send_res = communicator.send(Message::StartExecution).await;
-        assert!(send_res.is_ok(), "Send should be successful");
-        let recv_res = communicator.receive().await;
-        assert!(recv_res.is_ok(), "Receive should be successful");
-        let recv_msg = recv_res.unwrap();
-        assert!(matches!(recv_msg, Message::StartExecution));
-    }
+//     #[tokio::test]
+//     async fn test_send() {
+//         let mut communicator = ThreadCommunicator::<u32>::new().unwrap();
+//         let send_res = communicator.send(Message::StartExecution).await;
+//         assert!(send_res.is_ok(), "Send should be successful");
+//         let recv_res = communicator.receive().await;
+//         assert!(recv_res.is_ok(), "Receive should be successful");
+//         let recv_msg = recv_res.unwrap();
+//         assert!(matches!(recv_msg, Message::StartExecution));
+//     }
 
-    #[tokio::test]
-    async fn test_threaded_comm() {
-        let communicator = Arc::new(Mutex::new(ThreadCommunicator::<String>::new().unwrap()));
+//     #[tokio::test]
+//     async fn test_threaded_comm() {
+//         let communicator = Arc::new(Mutex::new(ThreadCommunicator::<String>::new().unwrap()));
 
-        let communicator_clone_1 = Arc::clone(&communicator);
-        let communicator_clone_2 = Arc::clone(&communicator);
-        let communicator_clone_3 = Arc::clone(&communicator);
+//         let communicator_clone_1 = Arc::clone(&communicator);
+//         let communicator_clone_2 = Arc::clone(&communicator);
+//         let communicator_clone_3 = Arc::clone(&communicator);
 
-        let sender_1_fn = move || {
-            let rt = Runtime::new().unwrap();
-            rt.block_on(async {
-                {
-                    let mut com = communicator_clone_1.lock().unwrap();
-                    let _ = com.send(Message::StartExecution).await;
-                    tracing::debug!("sending msg1 for the first time");
-                }
-                sleep(Duration::from_millis(10));
+//         let sender_1_fn = move || {
+//             let rt = Runtime::new().unwrap();
+//             rt.block_on(async {
+//                 {
+//                     let mut com = communicator_clone_1.lock().unwrap();
+//                     let _ = com.send(Message::StartExecution).await;
+//                     tracing::debug!("sending msg1 for the first time");
+//                 }
+//                 sleep(Duration::from_millis(10));
 
-                {
-                    let mut com = communicator_clone_1.lock().unwrap();
-                    let _ = com.send(Message::StartExecution).await;
-                    tracing::debug!("sending msg1 for the second time");
-                }
-                sleep(Duration::from_millis(10));
+//                 {
+//                     let mut com = communicator_clone_1.lock().unwrap();
+//                     let _ = com.send(Message::StartExecution).await;
+//                     tracing::debug!("sending msg1 for the second time");
+//                 }
+//                 sleep(Duration::from_millis(10));
 
-                {
-                    let mut com = communicator_clone_1.lock().unwrap();
-                    let _ = com.send(Message::StartExecution).await;
-                    tracing::debug!("sending msg1 for the third time");
-                }
-            });
-        };
+//                 {
+//                     let mut com = communicator_clone_1.lock().unwrap();
+//                     let _ = com.send(Message::StartExecution).await;
+//                     tracing::debug!("sending msg1 for the third time");
+//                 }
+//             });
+//         };
 
-        let sender_2_fn = move || {
-            let rt = Runtime::new().unwrap();
-            rt.block_on(async {
-                {
-                    let mut com = communicator_clone_2.lock().unwrap();
-                    let _ = com.send(Message::StopExecution).await;
-                    tracing::debug!("sending msg2 for the first time");
-                }
-                sleep(Duration::from_millis(10));
+//         let sender_2_fn = move || {
+//             let rt = Runtime::new().unwrap();
+//             rt.block_on(async {
+//                 {
+//                     let mut com = communicator_clone_2.lock().unwrap();
+//                     let _ = com.send(Message::StopExecution).await;
+//                     tracing::debug!("sending msg2 for the first time");
+//                 }
+//                 sleep(Duration::from_millis(10));
 
-                {
-                    let mut com = communicator_clone_2.lock().unwrap();
-                    let _ = com.send(Message::StopExecution).await;
-                    tracing::debug!("sending msg2 for the second time");
-                }
-                sleep(Duration::from_millis(10));
+//                 {
+//                     let mut com = communicator_clone_2.lock().unwrap();
+//                     let _ = com.send(Message::StopExecution).await;
+//                     tracing::debug!("sending msg2 for the second time");
+//                 }
+//                 sleep(Duration::from_millis(10));
 
-                {
-                    let mut com = communicator_clone_2.lock().unwrap();
-                    let _ = com.send(Message::StopExecution).await;
-                    tracing::debug!("sending msg2 for the third time");
-                }
-            });
-        };
+//                 {
+//                     let mut com = communicator_clone_2.lock().unwrap();
+//                     let _ = com.send(Message::StopExecution).await;
+//                     tracing::debug!("sending msg2 for the third time");
+//                 }
+//             });
+//         };
 
-        let recv_fn = move || {
-            let rt = Runtime::new().unwrap();
-            rt.block_on(async {
-                let start_time = Instant::now();
-                let timeout = Duration::from_secs(3);
-                let mut msg_1_counter: u8 = 0;
-                let mut msg_2_counter: u8 = 0;
-                while start_time.elapsed() < timeout {
-                    let res = {
-                        let mut com = communicator_clone_3.lock().unwrap();
-                        com.try_receive().await
-                    };
+//         let recv_fn = move || {
+//             let rt = Runtime::new().unwrap();
+//             rt.block_on(async {
+//                 let start_time = Instant::now();
+//                 let timeout = Duration::from_secs(3);
+//                 let mut msg_1_counter: u8 = 0;
+//                 let mut msg_2_counter: u8 = 0;
+//                 while start_time.elapsed() < timeout {
+//                     let res = {
+//                         let mut com = communicator_clone_3.lock().unwrap();
+//                         com.try_receive().await
+//                     };
 
-                    if let Ok(message) = res {
-                        match message {
-                            Some(Message::StartExecution) => {
-                                msg_1_counter += 1;
-                                tracing::debug!("received msg1");
-                                if msg_1_counter + msg_2_counter == 6 {
-                                    break;
-                                }
-                            }
-                            Some(Message::StopExecution) => {
-                                msg_2_counter += 1;
-                                tracing::debug!("received msg2");
-                                if msg_1_counter + msg_2_counter == 6 {
-                                    break;
-                                }
-                            }
-                            _ => {
-                                assert!(false)
-                            }
-                        }
-                    }
-                }
-                assert_eq!(msg_1_counter, 3);
-                assert_eq!(msg_2_counter, 3);
-            });
-        };
+//                     if let Ok(message) = res {
+//                         match message {
+//                             Some(Message::StartExecution) => {
+//                                 msg_1_counter += 1;
+//                                 tracing::debug!("received msg1");
+//                                 if msg_1_counter + msg_2_counter == 6 {
+//                                     break;
+//                                 }
+//                             }
+//                             Some(Message::StopExecution) => {
+//                                 msg_2_counter += 1;
+//                                 tracing::debug!("received msg2");
+//                                 if msg_1_counter + msg_2_counter == 6 {
+//                                     break;
+//                                 }
+//                             }
+//                             _ => {
+//                                 assert!(false)
+//                             }
+//                         }
+//                     }
+//                 }
+//                 assert_eq!(msg_1_counter, 3);
+//                 assert_eq!(msg_2_counter, 3);
+//             });
+//         };
 
-        let sender_1_handle = spawn(sender_1_fn);
-        let sender_2_handle = spawn(sender_2_fn);
-        let recv_handle = spawn(recv_fn);
+//         let sender_1_handle = spawn(sender_1_fn);
+//         let sender_2_handle = spawn(sender_2_fn);
+//         let recv_handle = spawn(recv_fn);
 
-        sender_1_handle.join().unwrap();
-        sender_2_handle.join().unwrap();
-        recv_handle.join().unwrap();
-    }
-}
+//         sender_1_handle.join().unwrap();
+//         sender_2_handle.join().unwrap();
+//         recv_handle.join().unwrap();
+//     }
+// }

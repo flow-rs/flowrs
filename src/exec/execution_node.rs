df9b650f9ecb5deaf38f3bdf0690a3e744e703c3
mophima <contact@moritzphilippmaier.de>
2025-05-19 16:10:15 +0200

    .

--- src/exec/execution_node.rs
+++ src/exec/execution_node.rs
@@ -21,7 +21,7 @@ use crate::{
     types::type_registry::POLL_REGISTRY,
 };
 #[cfg(target_arch = "wasm32")]
-use gloo::timers::future::yield_now;
+use gloo::futures::yield_now;
 
 pub struct ExecutionNode {
     execution_mode: ExecutionMode,
